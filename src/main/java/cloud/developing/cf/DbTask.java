e32a6a7d6b1ff70e1f29c78d1a13ecb4635662b5
Adam Smolnik <smolnik@wp.pl>
2016-12-07 00:56:02 +0100

    handling region as param

--- src/main/java/cloud/developing/cf/DbTask.java
+++ src/main/java/cloud/developing/cf/DbTask.java
@@ -14,18 +14,20 @@ import cloud.developing.core.Utils;
  */
 public class DbTask implements Task {
 
+	private static final String REGION_KEY = "region";
+
 	@Override
 	public Params execute(Params params) {
 		String template = Utils.readTemplateFile("mysql.yml");
 		String envName = "prod-us-1";
-		AmazonCloudFormation cf = Utils.getCF(params.getValue("region"));
-		cf.createStack(new CreateStackRequest().withParameters(params.getCFParams()).withStackName("db-" + envName)
+		AmazonCloudFormation cf = Utils.getCF(params.getValue(REGION_KEY));
+		cf.createStack(new CreateStackRequest().withParameters(params.getCFParamsExcept(REGION_KEY)).withStackName("db-" + envName)
 				.withTags(Utils.getProdTags(envName)).withTemplateBody(template));
 		return new Params();
 	}
 
 	public static void main(String[] args) {
-		Params params = new Params().with("region", Regions.US_EAST_1.getName()).with("envTypeValue", "prod").with("masterPassword", "xyz")
+		Params params = new Params().with(REGION_KEY, Regions.US_EAST_1.getName()).with("envTypeValue", "prod").with("masterPassword", "xyz123456")
 				.with("dbSecurityGroup", "sg-b0a18dca").with("dbSubnet1", "subnet-d06edf99").with("dbSubnet2", "subnet-53a06408");
 		new DbTask().execute(params);
 	}
